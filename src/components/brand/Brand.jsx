ca69a873f5698d724a6cde6acd82634eb8611143
CathyWong <1064348766@qq.com>
2024-05-10 14:55:16 +0800

    feat:brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import './brand.css';
-
+import React from "react";
+import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./import";
 function Brand() {
   return (
-    <div>Brand</div>
-  )
+    <div className="brand section-padding">
+        <img src={google} alt="google"></img>
+        <img src={slack} alt="slack"></img>
+        <img src={atlassian} alt="atlassian"></img>
+        <img src={dropbox} alt="dropbox"></img>
+        <img src={shopify} alt="shopify"></img>
+    </div>
+  );
 }
 
-export default Brand
+export default Brand;
